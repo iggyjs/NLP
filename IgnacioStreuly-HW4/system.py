b1413179304272d3dc5f181bccda28dadc11f2ea
Ignacio Streuly <ijstreuly@gmail.com>
2016-10-15 15:09:07 -0400

    Add: bigram table incorporation along with likelihood, ready to be written to file

--- IgnacioStreuly-HW4/system.py
+++ IgnacioStreuly-HW4/system.py
@@ -28,11 +28,49 @@ def main():
                 count_table[pos] += 1
             total_words += 1
 
-        print "Total words in corpus: " , total_words
-
         # likelihood_table population
         for key, value in count_table.iteritems():
             likelihood_table[key] = float(count_table[key])/total_words
+
+    count_table_words = {}
+    likelihood_table_words = {}
+
+    with open("training_corpus.pos") as infile:
+        for line in infile:
+            seq = line.split("\t")
+            if len(seq) < 2:
+                continue
+
+            value = seq[0]
+            pos = seq[1]
+            pos = pos.replace("\n", "")
+
+            if not value in count_table_words:
+                count_table_words[value] = {}
+                count_table_words[value][pos]=1
+
+            if value in count_table_words:
+                if not pos in count_table_words[value]:
+                    count_table_words[value][pos]=1
+                else:
+                    count_table_words[value][pos]+=1
+
+        # likelihood_table_words population
+        for key, value in count_table_words.iteritems():
+            if len(value) == 1:
+                likelihood_table_words[key] = {}
+                likelihood_table_words[key][value.keys()[0]]=1.0
+
+            elif len(value) > 1:
+                word = key
+                likelihood_table_words[word] = {}
+
+                pos_sums = 0
+                for key, val in value.iteritems():
+                    pos_sums += val
+
+                for key, val in value.iteritems():
+                    likelihood_table_words[word][key] = float(val)/pos_sums
 
 
     bigram_table = {}
@@ -74,7 +112,6 @@ def main():
         bigram_likelihood_table[key] = float(bigram_table[key])/total_lines_bigrams
 
 
-
     # populate prior_probability_table
 
     prior_probability_table = {}
@@ -94,17 +131,91 @@ def main():
     # HMM POS Tagging
 
     output = []
-
     with open("development_set.txt", "r+") as f:
         line_counter = 0
+        appends = 0
+
         for line in f:
+
             word = line.rstrip()
 
             if line_counter == 0:
                 most_likely_first = max(likelihood_table.iteritems(), key=operator.itemgetter(1))[0]
                 output.append([word, most_likely_first])
-            else:
-                
+                appends+=1
 
+            else:
+                try:
+                    if len(likelihood_table_words[word]) == 1:
+                        # assign it as the next pos
+                        pos = likelihood_table_words[word].keys()[0]
+                        output.append([word, pos])
+                        appends+=1
+
+                    #there are multiple occurences of this word as different POS tags
+                    elif len(likelihood_table_words[word]) > 1:
+                        # find the highest probability given the last POS tag: i.e.
+                        # for each of the POS tags this word appears as, multiply it by the prior probability including the last POS tag
+                        # and return the highest number.
+                        previous_pos_tag = output[len(output)-1][1]
+
+                        max_val = 0
+                        max_combination = []
+
+                        for key, value in likelihood_table_words[word].iteritems():
+                            check_max = calculate_max(key, value, previous_pos_tag, prior_probability_table)
+
+                            if check_max[1] > max_val:
+                                max_val = check_max[1]
+                                max_combination = check_max
+
+                        # max_combination contains the POS tag to add to output
+                        output.append([word, max_combination[0]])
+                        appends+=1
+
+                except:
+                    # use POS likelihood_table
+                    previous_pos_tag = output[len(output)-1][1]
+
+                    max_val = 0
+                    max_combination = []
+
+                    for key, value in likelihood_table.iteritems():
+                        check_max_exception = calculate_max_exception(key, value, previous_pos_tag, prior_probability_table)
+
+                        if check_max_exception[1] > max_val:
+                            max_val = check_max_exception[1]
+                            max_combination = check_max_exception
+
+
+                    output.append([word, max_combination[0]])
+                    appends+=1
+
+            line_counter+=1
+
+        print line_counter, " lines read."
+        print appends, " POS's appended to output"
+
+def calculate_max_exception(k, v, previous_pos_tag, prior_probability_table):
+    pos_document_probability = v
+    pos = k
+
+    search_key = (previous_pos_tag, pos)
+    try:
+        max_val = pos_document_probability * prior_probability_table[pos][search_key]
+    except:
+        max_val = pos_document_probability
+
+    return [pos, max_val]
+
+
+def calculate_max(k, v, previous_pos_tag, prior_probability_table):
+
+    likelihood_probability = v
+    pos = k
+    search_key = (previous_pos_tag, pos)
+
+    max_val = likelihood_probability * prior_probability_table[pos][search_key]
+    return [pos, max_val]
 
 main()
